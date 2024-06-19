91aa5cd612517c9beabccf6a218107383cd42d9e
Dietmar Heinke (Psychology) <d.g.heinke@bham.ac.uk>
2024-06-19 19:59:52 +0100

    init

---
+++ captcha/imkeys.php
@@ -0,0 +1,4 @@
+<?php
+	$oNameList = array("c3u","wvl","e7w","plt","85t","jl7","hep","xah","j4m","5hr");
+	$oCharList = array("X","4","3","C","Z","R","R","5","V","A");
+// End of file imkeys.php
---
+++ res/search.inc.php
@@ -0,0 +1,54 @@
+<?php
+
+/*****************
+	GENERAL SETTINGS
+*****************/
+$imSettings['search']['general'] = array(
+	'menu_position' => 'left',
+	'defaultScope' => array(
+		'0' => 'index.html',
+		'5' => 'teaching.html',
+		'6' => 'research-interests.html',
+		'8' => 'neural-network-benchmark.html',
+		'9' => '1---einleitung.html',
+		'3' => 'curriculum-vitae.html',
+		'13' => 'thank-you.html',
+		'11' => '2---modell-zur-visuellen-aufmerksamkeit.html',
+		'14' => '3---zeitliche-objektrepraesentation.html',
+		'15' => '4---selbstorganisation.html',
+		'16' => '5---neuronale-modellarchitektur.html',
+		'17' => 'network-model-of-binocular-interaction.html',
+		'19' => '1---zielstellung.html',
+		'20' => '2---texturfilter.html',
+		'21' => '3---aufbau-der-multiskalen-texturanalyse.html',
+		'22' => '4---texturfilter-1.ordnung.html',
+		'23' => '5---texturfilter-2.ordnung.html',
+		'24' => '6---zusammenfassung-und-literatur.html',
+		'26' => 'computer-science.html',
+		'27' => 'psychology.html'
+	),
+	'extendedScope' => array(
+	)
+);
+
+/*****************
+	PRODUCTS SEARCH
+*****************/
+$imSettings['search']['products'] = array(
+);
+
+/*****************
+	IMAGES SEARCH
+*****************/
+$imSettings['search']['images'] = array(
+);
+
+/*****************
+	VIDEOS SEARCH
+*****************/
+$imSettings['search']['videos'] = array(
+);
+$imSettings['search']['dynamicobjects'] = array(
+);
+
+// End of file search.inc.php
